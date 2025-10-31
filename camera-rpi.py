4a298b3270ab181248942c7d8a4844700c505c93
Harshavardhan S U <126003105@sastra.ac.in>
2025-10-31 09:56:05 +0530

    Implement photo capture and motion detection features
    Added functionality to capture a specified number of photos and capture images on motion detection.

--- camera-rpi.py
+++ camera-rpi.py
@@ -23,4 +23,43 @@ cam.contrast = 10
 default contrast = 0
 range: -100 to 100 '''
 
-#-----------------
+#--------------CAPTURING SPECIFIED NUMBER OF PHOTOS-------------------
+# PiCamera2 API - modern replacement for PiCamera, based on libcamera module
+# libcamera - modern linux camera framework
+from picamera2 import Picamera2
+from libcamera import Transform
+from time import time, sleep
+
+cam = Picamera2()
+n = int(input("How many photos? "))
+
+cam.start(show_preview=True, transform=Transform(vflip=True, hflip=True))
+
+for i in range(n):
+  cam.capture_file(f"/home/project/Pictures/image_{int(time())}.jpg")
+  sleep(2)
+  
+cam.close()
+
+#----------------CAPTURE PHOTO ON OBJECT DETECTION---------------------
+from picamera2 import Picamera2
+from libcamera import Transform
+from time import time, sleep
+from gpizero import MotionSensor
+
+cam = Picamera2()
+cam.configure
+cam.start()
+
+pir = MotionSensor(17) # BCM pin 17 = BOARD pin 11
+# event-driven style -- no infinite while loop 
+def capture_image():
+    filename = f"/home/project/Pictures/img_{int(time())}.jpg"
+    print("Motion detected! Capturing...")
+    picam2.capture_file(filename)
+    print("Saved:", filename)
+
+pir.when_motion = capture_image
+# The camera only captures when motion occurs -- non-blocking and elegant
+
+#----------------CAPTURE VIDEO ON OBJECT DETECTION---------------------
